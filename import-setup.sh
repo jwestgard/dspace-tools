fb91d0e798fa3f129fc9c959ad35fb3e49ee6563
Joshua Westgard <jwestgard@mac.com>
2016-02-03 15:32:35 -0500

    add import-setup.sh

---
+++ import-setup.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+#==============================================================================
+#
+#         FILE:  import-setup.sh
+#        USAGE:  import-setup.sh [file] [handle]
+#  DESCRIPTION:  Given a filename and DSpace handle, setup the directories 
+#                and files needed for adding the file to the object denoted
+#                by the handle using the dspace itemupdate command.
+#       AUTHOR:  Joshua A. Westgard
+#      CREATED:  2016.02.03
+#      VERSION:  1
+#
+#==============================================================================
+FILE=$1
+HANDLE=$2
+HOST=$(hostname)
+OUTDIR="load$(date +%Y%m%d)/item1"
+URL="http://$HOST:8084/$HANDLE"
+DC="$OUTDIR/dublin_core.xml"
+
+#----------------------------------------------------------------------
+# create directories and files, and move content file into place
+#----------------------------------------------------------------------
+echo "Setting up DSpace file import..."
+if [ ! -f $FILE ]; then
+  echo "Cannot locate specified file. Exiting."
+  exit 1
+fi
+mkdir -p "$OUTDIR"
+mv $FILE "$OUTDIR/$FILE"
+chmod +r "$OUTDIR/$FILE"
+echo "$FILE" > "$OUTDIR/contents" && echo "  - created contents file;"
+echo "$HANDLE" > "$OUTDIR/handle" && echo "  - created handle file;"
+
+#----------------------------------------------------------------------
+# create minimal dublin_core.xml containing URL of obj to be updated
+#----------------------------------------------------------------------
+echo '<?xml version="1.0" encoding="utf-8" standalone="no"?>' > "$DC"
+echo '<dublin_core schema="dc">' >> "$DC"
+echo "<dcvalue element=\"identifier\" qualifier=\"uri\">$URL</dcvalue>" >> "$DC"
+echo '</dublin_core>' >> "$DC"
+echo "  - created dublin_core.xml;"
+echo "Done."
